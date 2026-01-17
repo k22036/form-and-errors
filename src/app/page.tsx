fae98b25df838479d6698fa14eb1a650cfa1dbb3
k22036 <k22036kk@aitech.ac.jp>
2026-01-17 15:01:03 +0900

    fix: メインページのスタイルを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import MainPageList from "@/components/home/MainPageList";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-24 px-6 bg-white dark:bg-zinc-900 rounded-xl shadow-lg">
+      <main className="flex w-full max-w-3xl flex-col items-center justify-center py-24 px-6 bg-white dark:bg-zinc-900 rounded-xl shadow-lg">
         <h1 className="text-3xl font-bold mb-10 text-gray-900 dark:text-gray-100 text-center">
           テスト用ページ一覧
         </h1>
