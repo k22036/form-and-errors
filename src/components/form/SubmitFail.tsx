df63d3025d611858c3e66f59bc28de7581b970ba
k22036 <k22036kk@aitech.ac.jp>
2026-01-17 11:04:26 +0900

    fix: SubmitFailコンポーネントにrole属性を追加

--- src/components/form/SubmitFail.tsx
+++ src/components/form/SubmitFail.tsx
@@ -1,5 +1,9 @@
 const SubmitFail: React.FC = () => {
-  return <p className="text-red-500">送信に失敗しました。</p>;
+  return (
+    <p className="text-red-500" role="alert">
+      送信に失敗しました。
+    </p>
+  );
 };
 
 export default SubmitFail;
